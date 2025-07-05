b5b605bb71b0eb28a1b4feba0069216764947d58
Jaimin_Patel <jashdekavadiya@gmail.com>
2025-07-05 13:27:44 +0530

    🔴 Test: return correct sum when newlines are used as delimiters

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -13,4 +13,7 @@ describe('String Calculator', () => {
   it('returns the sum of multiple comma-separated numbers', () => {
     expect(add('1,2,3,4')).toBe(10);
   });
+  it('returns the sum when newlines are used as delimiters', () => {
+    expect(add('1\n2,3')).toBe(6);
+  });
 });
