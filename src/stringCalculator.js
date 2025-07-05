00f63924ceae6eb3b60cc1c93f395689651bdc1d
Jaimin_Patel <jashdekavadiya@gmail.com>
2025-07-05 13:17:15 +0530

    ♻️ Refactor: simplify logic using Array.reduce instad of for loop

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,10 +1,7 @@
 function add(numbers) {
   if (numbers === '') return 0;
   const parts = numbers.split(',');
-  var sum = 0;
-  for (let part of parts) {
-    sum += parseInt(part);
-  }
+  var sum = parts.reduce((acc, part) => acc + parseInt(part), 0);
   return sum;
 }
 
