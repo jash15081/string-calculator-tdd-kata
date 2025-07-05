c7335ade2300dd4eabad60d3678fa63d7a658d3a
Jaimin_Patel <jashdekavadiya@gmail.com>
2025-07-05 13:38:08 +0530

    ♻️ Refactor: replace parseInt with Array.map(Number)

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,7 +1,7 @@
 function add(numbers) {
   if (numbers === '') return 0;
-  const parts = numbers.replace(/\n/g, ',').split(',');
-  var sum = parts.reduce((acc, part) => acc + parseInt(part), 0);
+  const parts = numbers.replace(/\n/g, ',').split(',').map(Number);
+  var sum = parts.reduce((acc, part) => acc + part, 0);
   return sum;
 }
 
