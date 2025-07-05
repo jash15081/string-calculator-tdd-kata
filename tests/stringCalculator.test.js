5036ba2fc1758b54e4ca890a2deaba1b4080b536
Jaimin_Patel <jashdekavadiya@gmail.com>
2025-07-05 12:59:38 +0530

    🔴 Test: return the number itself when one number is provided

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -4,4 +4,7 @@ describe('String Calculator', () => {
   it('returns 0 for an empty string', () => {
     expect(add('')).toBe(0);
   });
+  it('returns the number itself if only one number is provided', () => {
+    expect(add('1')).toBe(1);
+  });
 });
